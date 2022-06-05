da3914c1c397864c15abcbd295ec823b0a2f826b
Huyne152 <1911549801@nttu.edu.vn>
2022-06-05 23:13:26 +0700

    update - code

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// Khai báo
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+const hbs = require('express-handlebars')
+const path  = require('path')
+
+
+//http logger 
+app.use(morgan('combined'))
+// template-engine
+app.engine('hbs', hbs.engine({
+  extname:'.hbs' 
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname,'resources','views'))
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/tintuc', (req, res) => {
+  res.render('tintuc')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
